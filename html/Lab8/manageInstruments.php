00b9fcd073fd4f942ebdf1495ecaf88cc9ccf9f1
miatomillo <mia.tomillo@centre.edu>
2023-11-07 19:19:26 +0000

    Lab 11 Complete

--- html/Lab8/manageInstruments.php
+++ html/Lab8/manageInstruments.php
@@ -4,10 +4,52 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// ----- TOGGLE DARK MODE -------------------------------------------------
+$mode = "user_mode";
+$light = "light";
+$dark = "dark";
+$button_pressed = "yes";
+$button_label =  "Toggle Dark/Light Modes";
+
+// ----- Initialize the dark/light mode cookie
+if (!isset($_COOKIE[$mode])){
+    setcookie($mode, $light, 0, "/", "", false, true);
+    header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+    exit();
+}
+
+// ----- Check if the toggle button was pressed
+if (isset($_POST[$button_pressed])){
+    $new_mode = $_COOKIE[$mode] == $light ? $dark : $light;
+    setcookie($mode, $new_mode, 0, "/", "", false, true);
+     header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+  exit();
+
+}
+
+session_start();
+if (!array_key_exists('num_deleted', $_SESSION)){
+         $_SESSION['num_deleted'] = 0;
+     }
+     
+     // ----- Logout
+if (isset($_POST['logout'])){
+    session_unset();
+    session_destroy();
+    header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+    exit(); 
+     }
+     
+     // ----- Store username
+     if (isset($_POST['username'])) {
+         $_SESSION['username'] = $_POST['username'];
+         header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+         exit(); 
+     }
+
+
 $config = parse_ini_file('/home/miatomillo/mysqli.ini');
 $dbname = 'instrument_rentals';
-
-
 
 if (!$conn = new mysqli($config['mysqli.default_host'],
     $config['mysqli.default_user'],
@@ -32,11 +74,15 @@ $did_delete = false;
 for($i = 0; $i < $select_result->num_rows; $i++){
     $id = $resrows[$i][0];
     $key = "checkbox" . $id;
+    
     if (isset($_POST[$key])){
         $did_delete = true;
-     if (!$del_stmt->execute()){
+        $did_suceed = $del_stmt->execute();
+     if (!$did_suceed){
         echo $conn->error;
     }
+    else
+    {$_SESSION['num_deleted']+=1;}
 }
 }
 
@@ -69,3 +115,44 @@ result_to_html_table($select_result);
 <input name="Insert" type="submit" value="Add Instruments">
 
 </form>
+
+<!DOCTYPE html>
+<?php
+       if ($_COOKIE[$mode] == $dark){
+       ?>
+           <link rel="stylesheet" href="darkmode.css">
+           
+       <?php
+       } else {
+       ?>
+           <link rel="stylesheet" href="basic.css">
+       <?php
+       }
+       ?>
+
+<form method=POST>
+   <input type="submit" name="<?= $button_pressed ?>" value="Toggle Light/Dark Mode"/>
+   </form>
+
+   <?php
+   if (isset($_SESSION['username'])) {
+   ?>
+       <p>Welcome <?php echo $_SESSION['username']; ?>!</p>
+       <form  method=POST>
+       <input type=submit name='logout' value="Logout"/>
+       </form>
+       <p>You have deleted <?php echo $_SESSION['num_deleted']; ?> Records.
+   <?php
+   } else {
+   ?>
+       <p>Remember my session:
+       <form method=POST>
+       <input type=text name='username' placeholder='Enter name...'/>
+       <input type=submit value='Remember Me'/>
+       </form>
+       
+   </p>
+   <?php
+   }
+   ?>
+</html>
